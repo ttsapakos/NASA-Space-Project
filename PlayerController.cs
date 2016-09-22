e0d2d592baac7dcf778b96b4de6a669f000a8be6
Theo Tsapakos <ttsapakos@gmail.com>
2016-09-21 23:02:12 -0400

    Initial game files

---
+++ PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float thrust;
+
+	private bool thruster;
+	private bool canCrash;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		thruster = false;
+		canCrash = false;
+	}
+
+	void updateThruster () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			thruster = true;
+			canCrash = true;
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			thruster = false;
+		}
+
+		if (thruster) {
+			rb.AddForce (new Vector2 (0, thrust));
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (canCrash) {
+			Debug.Log ("collision detected");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		updateThruster ();
+	}
+}
