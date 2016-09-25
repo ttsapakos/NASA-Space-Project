683c5afd4cf19459c0a7fd45251320a9846203ee
Theo Tsapakos <ttsapakos@gmail.com>
2016-09-25 13:25:17 -0400

    Updated movement files, ship now behaves appropriately

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,21 +5,32 @@ public class PlayerController : MonoBehaviour {
 
 	public Rigidbody2D rb;
 	public float thrust;
+	public float rotateMultiplier;
 
 	private bool thruster;
-	private bool canCrash;
+	private bool leftGround;
+	private bool rotateCCW;
+	private bool rotateCW;
+	private Transform planet;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 		thruster = false;
-		canCrash = false;
+		leftGround = false;
+		rotateCW = false;
+		rotateCCW = false;
+		planet = GameObject.FindGameObjectWithTag ("Earth").transform;
 	}
 
+	void applyGravityAndAirResistance () {
+		rb.AddForce((planet.position - transform.position).normalized * planet.localScale.x / 4);
+	}
+		
 	void updateThruster () {
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			thruster = true;
-			canCrash = true;
+			leftGround = true;
 		}
 
 		if (Input.GetKeyUp (KeyCode.Space)) {
@@ -27,18 +38,51 @@ public class PlayerController : MonoBehaviour {
 		}
 
 		if (thruster) {
-			rb.AddForce (new Vector2 (0, thrust));
+			// Applies force to the rocket at the right angle
+			float xThrust = Mathf.Sin(transform.rotation.eulerAngles.z * Mathf.Deg2Rad) * thrust * -1;
+			float yThrust = Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad) * thrust;
+			Vector2 force = new Vector3 (xThrust, yThrust);
+			rb.AddForce (force);
 		}
 	}
 
+	void updateRotation () {
+		
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			rotateCCW = true;
+		}
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			rotateCCW = false;
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			rotateCW = true;
+		}
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			rotateCW = false;
+		}
+
+		if (leftGround) {
+			if (rotateCW) {
+				transform.Rotate (new Vector3 (0, 0, -1 * rotateMultiplier));
+			}
+
+			if (rotateCCW) {
+				transform.Rotate (new Vector3 (0, 0, 1 * rotateMultiplier));
+			}
+		}
+
+	}
+
 	void OnCollisionEnter2D(Collision2D col) {
-		if (canCrash) {
+		if (leftGround) {
 			Debug.Log ("collision detected");
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
+		applyGravityAndAirResistance ();
 		updateThruster ();
+		updateRotation ();
 	}
 }
